d4b60490bdd47173d479a083bc3f97777bcd570c
Snehasish Guha Roy <snehasishguharoy871@gmail.com>
2021-07-12 05:00:15 +0530

    new files are added

--- src/main/java/com/example/highkeen/HelloCntlr.java
+++ src/main/java/com/example/highkeen/HelloCntlr.java
@@ -1,5 +1,6 @@
 package com.example.highkeen;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloCntlr {
 
+	@GetMapping
 	public String sayHello() {
 		return "Hello YouTube";
 	}
